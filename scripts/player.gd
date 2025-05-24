945807903e0e3c509117f1cfdd568e60a68be99e
Weng Xian Yang <e1398191@u.nus.edu>
2025-05-24 14:10:30 +0800

    added weapon
    fixed some bugs

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,9 +1,9 @@
 extends CharacterBody2D
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
-var CurrentItem : Item :
+@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
+@export var CurrentItem : Item :
 	set(value):
 		CurrentItem = value
-var StopRunning := false
 var InAnimation := false
 const SPEED = 130.0
 const JUMP_VELOCITY = -300.0
@@ -11,10 +11,8 @@ signal health_changed(NewHealth)
 @onready var collision_shape: CollisionShape2D = $CollisionShape2D
 @onready var timer: Timer = $Timer
 var Money:= 0
+var ComboCounter := 1
 signal HasDied()
-@onready var weaponmelee: Sprite2D = $Weaponmelee
-@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
-
 var MaxHealth := 100:
 	set(value):
 		MaxHealth = value
@@ -22,8 +20,11 @@ var CurrentHealth := 100:
 	set(value):
 		CurrentHealth = clamp(value, 0, MaxHealth)
 		health_changed.emit(CurrentHealth)
+@onready var marker: Marker2D = $Marker2D
 
 func _physics_process(delta: float) -> void:
+	#print(animation_player.current_animation)
+	
 	#death
 	if CurrentHealth <= 0:
 		HasDied.emit()
@@ -32,11 +33,7 @@ func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 	
-	if CurrentItem == null:
-		weaponmelee.visible = false
-	else:
-		weaponmelee.visible = true
-		
+
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
@@ -47,53 +44,38 @@ func _physics_process(delta: float) -> void:
 	
 	#flip the sprite
 	if direction > 0:
-		animated_sprite.flip_h = false
-		weaponmelee.flip_h = false
+		marker.scale.x = 1
 	elif direction < 0:
-		animated_sprite.flip_h = true
-		weaponmelee.flip_h = true
+		marker.scale.x = -1
+	
 	#play animation
 	if !InAnimation:
 		if is_on_floor():
-			if direction == 0 && !StopRunning: 
-				if CurrentItem == null :
-					animated_sprite.play("idle")
-				else:
-					animation_player.play("idle")
-			elif direction != 0 :
-				animated_sprite.play("run")
-				StopRunning = true
-			else :
-				animated_sprite.play("endrun")
+			if direction == 0: 
+				animation_player.play("idle")
+			else:
+				animation_player.play("run")
 		else:
-			animated_sprite.play("jump")
+			animation_player.play("jump")
 		
-		if direction:
-			velocity.x = direction * SPEED
-		else:
-			velocity.x = move_toward(velocity.x, 0, SPEED)
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
 	
 	move_and_slide()
 	
-	if Input.is_action_pressed("attack"):
-		InAnimation = true
-		animated_sprite.play("icestrike")
-
-func _on_animated_sprite_2d_animation_finished() -> void:
-	if animated_sprite.animation == "endrun":
-		StopRunning = false
-	if animated_sprite.animation == "icestrike":
-		InAnimation = false
-
+	if Input.is_action_just_pressed("attack"):
+		if !InAnimation:
+			InAnimation = true
+			AttackAnimation()
 
 func _on_health_changed(NewHealth: Variant) -> void:
 	print(NewHealth)
 
-
 func _on_timer_timeout() -> void:
 	Engine.time_scale = 1
 	get_tree().reload_current_scene()
-
 
 func _on_has_died() -> void:
 	Engine.time_scale = 0.5
@@ -101,3 +83,21 @@ func _on_has_died() -> void:
 		print("you died")
 		collision_shape.queue_free()
 		timer.start()
+
+
+func _on_animation_player_animation_finished(anim_name: StringName) -> void:
+	InAnimation = false
+	#print(anim_name)
+
+func AttackAnimation():
+	if CurrentItem != null:
+		animation_player.play(CurrentItem.animation + Combo(CurrentItem.animation))
+		ComboCounter += 1
+	if ComboCounter > 3:
+		ComboCounter = 1
+
+func Combo(animation):
+	if animation in ["Ruler"]:
+		return "_" + str(ComboCounter)
+	else:
+		return ""
